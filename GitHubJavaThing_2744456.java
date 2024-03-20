2325ee92804973b4fbb598bca67768556752b677
FutureProduct <162730806+FutureProduct@users.noreply.github.com>
2024-03-20 11:43:58 -0400

    Add files via upload

---
+++ GitHubJavaThing_2744456.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubjavathing_2744456;
+
+/**
+ *
+ * @author gamer
+ */
+public class GitHubJavaThing_2744456 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        int a = 3;
+        int b = 2;
+        System.out.print(subtraction(a,b));
+    }
+    
+    public static int subtraction(int a, int b)
+    {
+        return a - b;
+    }
+    
+}
