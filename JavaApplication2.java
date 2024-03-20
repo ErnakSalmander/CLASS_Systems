4065c023772f7ee7fe4740548f87b833c4b077bf
ErnakSalmander <163885011+ErnakSalmander@users.noreply.github.com>
2024-03-20 11:45:34 -0400

    Add files via upload

---
+++ JavaApplication2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author rusla
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+      addition();
+        
+    }
+
+    
+       
+    
+public int addition(int a, int b) {
+    a= 5;
+    b= 5;
+    return a+b;
+}
+        
+    }
+    
+
